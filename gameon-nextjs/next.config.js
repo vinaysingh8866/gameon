5e394f05b1438b3c0b47bc09303ae736f8d263d8
Vinay Singh <vinaysingh8866@gmail.com>
2023-03-16 13:20:29 -0400

    init

---
+++ gameon-nextjs/next.config.js
@@ -0,0 +1,30 @@
+const { withNativebase } = require("@native-base/next-adapter");
+const path = require("path");
+
+module.exports = withNativebase({
+  dependencies: ["@native-base/icons"],
+
+  nextConfig: {
+    webpack: (config, options) => {
+      config.module.rules.push({
+        test: /\.ttf$/,
+        loader: "url-loader", // or directly file-loader
+        include: path.resolve(__dirname, "node_modules/@native-base/icons"),
+      });
+      config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        "react-native$": "react-native-web",
+      };
+      config.resolve.extensions = [
+        ".web.js",
+        ".web.ts",
+        ".web.tsx",
+        ...config.resolve.extensions,
+      ];
+      return config;
+    },
+    images: {
+      unoptimized: true,
+    },
+  },
+});

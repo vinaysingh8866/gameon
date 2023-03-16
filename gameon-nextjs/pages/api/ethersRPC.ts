26a2611591b5e0829fa7adf41cf8b83227e9ea56
Vinay Singh <vinaysingh8866@gmail.com>
2023-03-16 18:13:51 -0400

    funtions to call blockchain

--- gameon-nextjs/pages/api/ethersRPC.ts
+++ gameon-nextjs/pages/api/ethersRPC.ts
@@ -22,4 +22,83 @@ export default class EthereumRpc {
     }
   }
 
+  async mintNFT(url: any): Promise<any> {
+    try {
+      const address = await this.getAccounts();
+      const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+      const signer = ethersProvider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const tx = await contract.safeMint(address, url, {
+        value: ethers.utils.parseEther("0.1")
+      });
+      console.log("tx: ", tx);
+      const receipt = await tx.wait();
+      return receipt;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getPrice(): Promise<any> {
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+      const signer = ethersProvider.getSigner();
+      const contract = new ethers.Contract(contractAddress, abi, signer);
+      const price = await contract.PRICE();
+      const priceInEth = await this.convertToEthFromWei(price);
+      return priceInEth;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async convertToEthFromWei(wei: BigNumberish): Promise<any> {
+    try {
+      const eth = ethers.utils.formatEther(wei);
+      return eth;
+    }
+    catch (error) {
+      return error;
+    }
+  }
+
+
+
+  async getAccounts(): Promise<any> {
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+      const signer = ethersProvider.getSigner();
+      const address = await signer.getAddress();
+      return address;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  async getBalance(): Promise<string> {
+    try {
+      const ethersProvider = new ethers.providers.Web3Provider(this.provider);
+      const signer = ethersProvider.getSigner();
+      const address = await signer.getAddress();
+      const balance = ethers.utils.formatEther(
+        await ethersProvider.getBalance(address)
+      );
+      return balance;
+    } catch (error) {
+      return error as string;
+    }
+  }
+
+
+  async getPrivateKey(): Promise<any> {
+    try {
+      const privateKey = await this.provider.request({
+        method: "eth_private_key",
+      });
+
+      return privateKey;
+    } catch (error) {
+      return error as string;
+    }
+  }
 }
